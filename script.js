6b083a553dabb322b2dc5a21261836ffe6de5c97
kavya191 <kavyabose191@gmail.com>
2023-06-20 11:09:28 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,16 @@
+function displayData(data){
+    calcInput.value =calcInput.value+data
+}
+
+function allClear(){
+    calcInput.value=""
+}
+//eval is an inbult method
+function outputOperator(){
+   calcInput.value=eval( calcInput.value)
+}
+
+function displayClear(){
+    // already display cheythittulla valuesil ninnu last index value remove cheyyan slice method use cheyyunnu
+    calcInput.value=calcInput.value.slice(0,-1)
+}
